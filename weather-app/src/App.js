1d7ec4a297dc2b16b79fcde2e7bb05dae4f14f0b
y-arimura1222 <y.arimura.carecon@gmail.com>
2021-08-28 00:21:16 +0900

    単純なAPI呼び出し

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,4 +1,24 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
 const App = () => {
+
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios(
+        `https://api.openweathermap.org/data/2.5/weather?zip=899-5541,jp&appid=8f241f6e111e93a94a517a3c6477329e&lang=ja&units=metric`,
+      );
+
+      setData(result.data);
+    };
+
+    fetchData();
+  }, []);
+
+  console.log(data);
+
   return (
     <div className="App">
       Hello React
