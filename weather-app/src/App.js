583235baf95cc3c59ddd9dcc5bc23f9d4f5a3c9f
y-arimura1222 <y.arimura.carecon@gmail.com>
2021-08-28 23:47:49 +0900

    API呼び出しファイルを分けた

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,29 +1,11 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
+import React from 'react'
+import { Input } from './components/Input';
 
-const App = () => {
-
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const result = await axios(
-        `https://api.openweathermap.org/data/2.5/weather?zip=899-5541,jp&appid=8f241f6e111e93a94a517a3c6477329e&lang=ja&units=metric`,
-      );
-
-      setData(result.data);
-    };
-
-    fetchData();
-  }, []);
-
-  console.log(data);
+export const App = () => {
 
   return (
-    <div className="App">
-      Hello React
-    </div>
+    <>
+      <Input />
+    </>
   );
 }
-
-export default App;
---
+++ weather-app/src/components/Input.jsx
@@ -0,0 +1,49 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import { Place } from './Place';
+import { Main } from './Main';
+
+export const Input = () => {
+
+  const [data, setData] = useState({});
+  const [text, setText] = useState('');
+  const [zipCode, setZipCode] = useState('170-0012')
+  const [isLoading, setIsLoading] = useState(false)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios(
+        `https://api.openweathermap.org/data/2.5/weather?zip=${zipCode},jp&appid=8f241f6e111e93a94a517a3c6477329e&lang=ja&units=metric`,
+      );
+
+      setData(result.data);
+    };
+    fetchData();
+    setIsLoading(false);
+  }, [zipCode]);
+
+  const onChangeText = (e) => {
+    setText(e.target.value);
+  }
+
+  const onClickGetCode = () => {
+    setZipCode(text);
+  }
+
+  return (
+    <>
+      {
+        isLoading ? (
+          <p>Loading...</p>
+        ) : (
+          <>
+            <input onChange={onChangeText} />
+            <button onClick={onClickGetCode}>GET</button>
+            <Place data={data} />
+            <Main data={data} />
+          </>
+        )
+      }
+    </>
+  )
+}
