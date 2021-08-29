07fd1990795aa004d2676382d6c68c2353f0698c
y-arimura1222 <y.arimura.carecon@gmail.com>
2021-08-29 13:37:35 +0900

    Temp components style 修正

--- weather-app/src/components/ui/Main.jsx
+++ weather-app/src/components/ui/Main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import styled from 'styled-components';
 
-const Container = styled.h2`
+const Container = styled.div`
   text-align: center;
 `
 
--- weather-app/src/components/ui/Temp.jsx
+++ weather-app/src/components/ui/Temp.jsx
@@ -1,14 +1,30 @@
 import React from 'react'
+import styled from 'styled-components';
+
+const Container = styled.div`
+  text-align: center;
+  display: flex;
+  justify-content: space-evenly;
+  p.temp_min {
+    color: blue;
+  }
+  p.temp_max {
+    color: red;
+  }
+  p.feels_like {
+    color: green;
+  }
+`
 
 export const Temp = (props) => {
 
   const { temp_min, temp_max, feels_like } = props;
 
   return (
-    <>
-      <p>{`temp_min : ${temp_min}`}</p>
-      <p>{`temp_max : ${temp_max}`}</p>
-      <p>{`feels_like : ${feels_like}`}</p>
-    </>
+    <Container>
+      <p className="temp_min">{`最低 ${temp_min}℃`}</p>
+      <p className="temp_max">{`最高 ${temp_max}℃`}</p>
+      <p className="feels_like">{`体感 ${feels_like}℃`}</p>
+    </Container>
   )
 }
