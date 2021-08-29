96ba76d98d93bc18f078da215a6aa315e08d0aad
y-arimura1222 <y.arimura.carecon@gmail.com>
2021-08-29 12:06:37 +0900

    place styled

--- weather-app/src/components/ui/Place.jsx
+++ weather-app/src/components/ui/Place.jsx
@@ -1,4 +1,11 @@
 import React from 'react'
+import styled from 'styled-components';
+
+const CityName = styled.h2`
+  text-align: center;
+  font-size: 24px;
+  padding: 16px 0;
+`
 
 export const Place = (props) => {
 
@@ -8,7 +15,7 @@ export const Place = (props) => {
 
   return (
     <>
-      <p>{data.name}</p>
+      <CityName>{data.name}</CityName>
     </>
   )
 }
