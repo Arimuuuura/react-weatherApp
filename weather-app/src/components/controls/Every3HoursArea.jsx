5c82a2c823952fa9ff25d0982e3261d8ef1adfef
y-arimura1222 <y.arimura.carecon@gmail.com>
2021-09-17 23:06:46 +0900

    Every3HoursArea style 修正

--- weather-app/src/components/controls/Every3HoursArea.jsx
+++ weather-app/src/components/controls/Every3HoursArea.jsx
@@ -1,5 +1,41 @@
 import React from 'react'
+import styled from 'styled-components';
 import { useGetData } from '../../function/getData';
+
+const Container = styled.div`
+  display: flex;
+  background-color: rgba(0, 128, 128, 0.5);
+  font-size: 16px;
+`
+
+const TitleUl = styled.ul`
+  text-align: center;
+  & li {
+    width: 56px;
+    box-sizing: border-box;
+  }
+  & span {
+    display: block;
+    height: 40px;
+    line-height: 40px;
+  }
+`
+
+const ContainerResult = styled.div`
+  overflow: scroll;
+  display: flex;
+`
+
+const ResultUl = styled.ul`
+  width: 64px;
+  text-align: center;
+`
+
+const Icon = styled.img`
+  width: 40px;
+  display: block;
+  margin: 0 auto;
+`
 
 export const Every3HoursArea = () => {
 
@@ -10,13 +46,15 @@ export const Every3HoursArea = () => {
   // todo cod を使ってレスポンス結果の出し分け実装 cod : 200 or 404
 
   const kinds = list.map((val, index) => {
-    const dt = val.dt;
-    const temp = val.main.temp;
-    const humidity = val.main.humidity;
-    const icon = val.weather[0].icon;
-    const speed = val.wind.speed;
+    const id = index;
+    const dt = `${new Date(val.dt * 1000).getHours()}時`;
+    const temp = `${val.main.temp}℃`;
+    const humidity = `${val.main.humidity}%`;
+    const icon = <Icon src={`http://openweathermap.org/img/wn/${val.weather[0].icon}@2x.png`} alt="アイコン" />;
+    const speed = `${val.wind.speed}m/s`;
 
     return {
+      id,
       dt,
       temp,
       humidity,
@@ -25,54 +63,32 @@ export const Every3HoursArea = () => {
     }
   })
 
-  // todo 一回の map 実行でできないか
+  console.log(kinds);
+
   return (
-    <table>
-      <tbody>
-        <tr>
-          <th>時間</th><td>0時</td>
-        </tr>
-        <tr>
-          <th>天気</th>
-          {
-            kinds.map((val, index) => (
-              index < 8 ? (
-                <td key={index}><img src={`http://openweathermap.org/img/wn/${val.icon}@2x.png`} alt="アイコン" /></td>
-              ) : null
-            ))
-          }
-        </tr>
-        <tr>
-          <th>気温</th>
+    <Container>
+      <TitleUl>
+        <li>時間</li>
+        <li><span>天気</span></li>
+        <li>気温</li>
+        <li>湿度</li>
+        <li>風速</li>
+      </TitleUl>
+      <ContainerResult>
         {
-          kinds.map((val, index) => (
-            index < 8 ? (
-              <td key={index}>{val.temp}℃</td>
+          kinds.map((val) => (
+            val.id < 8 ? (
+              <ResultUl>
+                <li key={val.id}>{val.dt}</li>
+                <li key={val.id}>{val.icon}</li>
+                <li key={val.id}>{val.temp}</li>
+                <li key={val.id}>{val.humidity}</li>
+                <li key={val.id}>{val.speed}</li>
+              </ResultUl>
             ) : null
           ))
         }
-        </tr>
-        <tr>
-          <th>湿度</th>
-          {
-            kinds.map((val, index) => (
-              index < 8 ? (
-                <td key={index}>{val.humidity}%</td>
-              ) : null
-            ))
-          }
-        </tr>
-        <tr>
-          <th>風速</th>
-          {
-            kinds.map((val, index) => (
-              index < 8 ? (
-                <td key={index}>{val.speed}m/s</td>
-              ) : null
-            ))
-          }
-        </tr>
-      </tbody>
-    </table>
+      </ContainerResult>
+    </Container>
   )
 }
