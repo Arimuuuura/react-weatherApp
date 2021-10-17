7353a4d6eef546bbd886fd06180a9b9efd3ed7b5
y-arimura1222 <y.arimura.carecon@gmail.com>
2021-10-17 16:47:28 +0900

    Loading icon の導入

---
+++ weather-app/src/components/main/components/Loading.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import styled from 'styled-components'
+import spinner from '../../images/spinner.gif'
+
+const LoadingText = styled.p`
+  width: 200px;
+  margin: 0 auto;
+  padding: 80px 0 0;
+  text-align: center;
+  font-size: 24px;
+`
+
+const Spinner = styled.img`
+  width: 100px;
+  padding: 50px;
+  margin: 0 auto;
+  display: block;
+`
+
+export const Loading = () => {
+  return (
+    <>
+      <LoadingText>Loading..</LoadingText>
+      <Spinner src={ spinner } alt='Loading' />
+    </>
+  )
+}
